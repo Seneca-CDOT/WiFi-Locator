ac47d3f5ab372e9997d42e554f0fa1c5e04bf17c
Ethan Childerhose <ethchil@australia.cdot.systems>
2017-08-02 16:29:15 -0400

    Changed to ArrayList

--- ProofOfConcept/src/Main.java
+++ ProofOfConcept/src/Main.java
@@ -7,6 +7,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class Main {
 
@@ -27,31 +28,44 @@ public class Main {
 			public void actionPerformed(ActionEvent e) {
 				System.out.println("button pressed");
 				System.out.println(getInfo("Access")); // iwconfig wlp2s0| grep "Signal"
-
-				StringBuilder sbaddress = new StringBuilder(getInfo("Access")); //getting access point
-				for (int i = 0; i < 27; i++) {
-					sbaddress.deleteCharAt(0);
-				}
-				String address = sbaddress.toString();
-				System.out.println(address);
 				
-				StringBuilder sbsignal = new StringBuilder(getInfo("Signal")); //getting signal strength
-				for (int i = 0; i < 10; i++) {
-					sbsignal.deleteCharAt(0);
+				ArrayList<String> Address = new ArrayList<String>();
+				ArrayList<String> Signal = new ArrayList<String>();
+				Address = getInfo("Access");
+				Signal = getInfo("Signal");
+				
+				
+				for(int i = 0; i < Signal.size(); i++)
+				{
+					StringBuilder sbaddress = new StringBuilder(Address.get(i)); //getting access point
+
+					sbaddress.delete(0, 59);
+					String address = sbaddress.toString();
+					System.out.println(address);
 				}
-				String signal = sbsignal.toString();
-				System.out.println(signal);
+				
+				for(int i = 0; i < Signal.size(); i++)
+				{
+					StringBuilder sbsignal = new StringBuilder(Signal.get(i)); //getting signal strength
+
+					sbsignal.delete(0, 43);
+					sbsignal.delete(3, 7);
+					String signal = sbsignal.toString();
+					System.out.println(signal);
+				}
 				
 			}
 		});
 
 	}
 
-	public static String getInfo(String command) {
+	public static ArrayList<String> getInfo(String command) {
 		String s = null;
+		ArrayList<String> output = new ArrayList<String>();
 
 		try {
-
+			
+			
 			// run the terminal command
 			// using the Runtime exec method:
 			Process p = Runtime.getRuntime().exec("iwconfig wlp2s0");
@@ -61,18 +75,19 @@ public class Main {
 			BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
 
 			// read the output from the command
-			System.out.println("Here is the standard output of the command:\n");
+			//System.out.println("Here is the standard output of the command:\n");
 			while ((s = stdInput.readLine()) != null) {
 				if(s.contains(command))
 				{
-					return s;
+					output.add(output.size()+1, s);
+					
 				}
 			}
 
 			// read any errors from the attempted command
 			//System.out.println("Here is the standard error of the command (if any):\n");
 			while ((s = stdError.readLine()) != null) {
-				return s;
+				return output;
 			}
 
 			System.exit(0);
@@ -82,7 +97,7 @@ public class Main {
 			e.printStackTrace();
 			System.exit(-1);
 		}
-		return s;
+		return output;
 
 	}
 
