1fa7aa3fdd780c4c59c1e16fbd4c6457047ad55d
Samantha Yao <yaosa@newzealand.cdot.systems>
2017-08-02 15:34:55 -0400

    prints address and signal

--- ProofOfConcept/src/Main.java
+++ ProofOfConcept/src/Main.java
@@ -6,8 +6,6 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-import android.content.Context;
-import android.net.wifi.WifiManager;
 import java.io.*;
 
 public class Main {
@@ -17,66 +15,73 @@ public class Main {
 		JFrame window = new JFrame("Create Nodes");
 		JButton saveNode = new JButton();
 		JPanel pane = new JPanel();
-		
+
 		window.setSize(500, 500);
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setVisible(true);
 		window.add(pane);
-		saveNode.setPreferredSize(new Dimension (100,40));
+		saveNode.setPreferredSize(new Dimension(100, 40));
 		pane.add(saveNode);
-		
-		
+
 		saveNode.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				System.out.println("button pressed");
-				System.out.println(getInfo("iwconfig")); //iwconfig wlp2s0 grep "Signal"
-				getInfo("iwconfig wlp2s0 grep \"Address\"");
-				getInfo("iwconfig wlp2s0 grep \"\"");
+				System.out.println(getInfo("iwconfig wlp2s0| grep \"Access\"")); // iwconfig wlp2s0| grep "Signal"
+				getInfo("iwconfig wlp2s0| grep \"Signal\"");
+
+				StringBuilder sbaddress = new StringBuilder(getInfo("iwconfig wlp2s0| grep \"Access\"")); //getting access point
+				for (int i = 0; i < 49; i++) {
+					sbaddress.deleteCharAt(0);
+				}
+				String address = sbaddress.toString();
+				System.out.println(address);
+				
+				StringBuilder sbsignal = new StringBuilder(getInfo("iwconfig wlp2s0| grep \"Signal\"")); //getting signal strength
+				for (int i = 0; i < 33; i++) {
+					sbsignal.deleteCharAt(0);
+				}
+				String signal = sbsignal.toString();
+				System.out.println(signal);
+				
 			}
 		});
-		
-		
-		
-		
+
 	}
-	
+
 	public static String getInfo(String command) {
 		String s = null;
 
-        try {
-            
-	    // run the iwconfig wlp2s0 command
-            // using the Runtime exec method:
-            Process p = Runtime.getRuntime().exec(command); 
-            
-            BufferedReader stdInput = new BufferedReader(new 
-                 InputStreamReader(p.getInputStream()));
+		try {
 
-            BufferedReader stdError = new BufferedReader(new 
-                 InputStreamReader(p.getErrorStream()));
+			// run the terminal command
+			// using the Runtime exec method:
+			Process p = Runtime.getRuntime().exec(command);
 
-            // read the output from the command
-            System.out.println("Here is the standard output of the command:\n");
-            while ((s = stdInput.readLine()) != null) {
-                return s;
-            }
-            
-            // read any errors from the attempted command
-            System.out.println("Here is the standard error of the command (if any):\n");
-            while ((s = stdError.readLine()) != null) {
-                return s;
-            }
-            
-            System.exit(0);
-          
-        }
-        catch (IOException e) {
-            System.out.println("Exception occurred.");
-            e.printStackTrace();
-            System.exit(-1);
-        }
+			BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+			BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+
+			// read the output from the command
+			System.out.println("Here is the standard output of the command:\n");
+			while ((s = stdInput.readLine()) != null) {
+				return s;
+			}
+
+			// read any errors from the attempted command
+			//System.out.println("Here is the standard error of the command (if any):\n");
+			while ((s = stdError.readLine()) != null) {
+				return s;
+			}
+
+			System.exit(0);
+
+		} catch (IOException e) {
+			System.out.println("Exception occurred.");
+			e.printStackTrace();
+			System.exit(-1);
+		}
 		return s;
-		
+
 	}
 
 }
