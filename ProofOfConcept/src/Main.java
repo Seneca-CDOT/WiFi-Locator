1d8aa6d7b9accfaa21a68c70c1957d1fb7796d85
Samantha Yao <yaosa@newzealand.cdot.systems>
2017-08-02 13:36:29 -0400

    wireless setup

--- ProofOfConcept/src/Main.java
+++ ProofOfConcept/src/Main.java
@@ -5,6 +5,10 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import android.content.Context;
+import android.net.wifi.WifiManager;
+import java.io.*;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -26,6 +30,40 @@ public class Main {
 				System.out.println("button pressed");
 			}
 		});
+		
+		String s = null;
+
+        try {
+            
+	    // run the iwconfig wlp2s0 command
+            // using the Runtime exec method:
+            Process p = Runtime.getRuntime().exec("iwconfig");
+            
+            BufferedReader stdInput = new BufferedReader(new 
+                 InputStreamReader(p.getInputStream()));
+
+            BufferedReader stdError = new BufferedReader(new 
+                 InputStreamReader(p.getErrorStream()));
+
+            // read the output from the command
+            System.out.println("Here is the standard output of the command:\n");
+            while ((s = stdInput.readLine()) != null) {
+                System.out.println(s);
+            }
+            
+            // read any errors from the attempted command
+            System.out.println("Here is the standard error of the command (if any):\n");
+            while ((s = stdError.readLine()) != null) {
+                System.out.println(s);
+            }
+            
+            System.exit(0);
+        }
+        catch (IOException e) {
+            System.out.println("Exception occurred.");
+            e.printStackTrace();
+            System.exit(-1);
+        }
 		
 		
 	}
