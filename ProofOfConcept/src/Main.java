f96c405ce40870b9e45c2da6fda04044c7657123
Ethan Childerhose <ethchil@australia.cdot.systems>
2017-08-02 13:20:34 -0400

    Added GUI

--- ProofOfConcept/src/Main.java
+++ ProofOfConcept/src/Main.java
@@ -1,9 +1,33 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		JFrame window = new JFrame("Create Nodes");
+		JButton saveNode = new JButton();
+		JPanel pane = new JPanel();
+		
+		window.setSize(500, 500);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setVisible(true);
+		window.add(pane);
+		pane.add(saveNode);
+		
+		saveNode.setSize(200, 200);
+		
+		saveNode.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.out.println("button pressed");
+			}
+		});
+		
+		
 	}
 
 }
