523a2d19503a87ac3c57d1c2912b00dff7751cb7
Ethan Childerhose <ethchil@australia.cdot.systems>
2017-08-10 16:08:02 -0400

    Added Time to Node

--- ProofOfConcept/src/Node.java
+++ ProofOfConcept/src/Node.java
@@ -1,18 +1,19 @@
 import java.util.List;
 
 public class Node {
+	int timeStamp;
 	int NodeNum;
 	List<String> MacAddr;
 	List<String> NodeStrength;
 	List<String> SSID;
 	
-	public Node(int Nodenum, List<String> Mac, List<String> Strength, List<String> ID)
+	public Node(int Time, int Nodenum, List<String> Mac, List<String> Strength, List<String> ID)
 	{
+		timeStamp = Time;
 		NodeNum = Nodenum;
 		MacAddr = Mac;
 		NodeStrength = Strength;
 		SSID = ID;
-		
 	}
 	
 
