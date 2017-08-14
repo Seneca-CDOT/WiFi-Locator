a8848390bc568898cbab0d1e30e766a0cc30d981
Samantha Yao <yaosa@utschools.ca>
2017-08-14 15:33:57 -0400

    added node

---
+++ ProofOfConcept/src/CSVAnalyzer/Node.java
@@ -0,0 +1,21 @@
+package CSVAnalyzer;
+
+import java.util.List;
+
+/**
+ * @author sammydamdam
+ */
+public class Node {
+	int NodeNum;
+	List<String> MacAddrs;
+	List<String> NodeSignal;
+	List<String> SSID;
+
+	public Node(int Nodenum, List<String> Mac, List<String> Strength, List<String> ID) {
+		NodeNum = Nodenum;
+		MacAddrs = Mac;
+		NodeSignal = Strength;
+		SSID = ID;
+	}
+
+}
