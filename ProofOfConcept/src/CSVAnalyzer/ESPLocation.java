fafb3c99024d672c77445bc5bcc7946c59e0b057
Samantha Yao <yaosa@utschools.ca>
2017-08-15 10:26:20 -0400

    added esp location

---
+++ ProofOfConcept/src/CSVAnalyzer/ESPLocation.java
@@ -0,0 +1,24 @@
+package CSVAnalyzer;
+
+import java.util.List;
+
+/**
+ * 
+ * @author sammydamdam
+ */
+public class ESPLocation {
+	long timestamp;
+	String mac;
+	List<String> SSID;
+	List<String> MacAddrs;
+	List<String> Signal;
+
+	public ESPLocation(long time, String MACAddress, List<String> ssid, List<String> mac, List<String> signal) {
+		timestamp = time;
+		this.mac = MACAddress;
+		this.SSID = ssid;
+		this.MacAddrs = mac;
+		this.Signal = signal;
+	}
+
+}
